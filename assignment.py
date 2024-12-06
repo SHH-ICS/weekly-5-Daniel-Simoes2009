aea572e12b2af2c3df6fb08b38a9e436d4c2db47
Daniel Simoes <daniel.simoes@stu.ocsb.ca>
2024-12-06 09:35:19 -0500

    Update assignment.py 2

--- assignment.py
+++ assignment.py
@@ -4,7 +4,7 @@ pi_value = 0
 for n in range(num_iterations):
     term = 4 / (2 * n + 1)
     if n % 2 == 0:
-        term = lerm
+        term = term
     else:
         term = -term
         pi_value += term
