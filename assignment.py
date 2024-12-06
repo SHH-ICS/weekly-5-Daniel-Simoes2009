82182d5fad98bcdc39c5a694fd17091aee056379
Daniel Simoes <daniel.simoes@stu.ocsb.ca>
2024-12-06 09:24:15 -0500

    Update assignment.py
    finally

--- assignment.py
+++ assignment.py
@@ -1,6 +1,11 @@
-def calculatePi(digits):
-  result = 0
-  return result
-
-number_of_digits = input()
-print(calculatePi(number_of_digits))
+#does not seem to work for some reason 
+num_iterations = int(input("Enter the number of iterations: "))
+pi_value = 0
+for n in range(num_iterations):
+    term = 4 / (2 * n + 1)
+    if n % 2 == 0:
+        term = lerm
+    else:
+        term = -term
+        pi_value += term
+print("The value of pi is:",pi_value)
